df0041b13157d1f6c89acfb22c504187e6bffe23
ssirpatil <ssirpatil@82b4b892-f3ff-11dd-802b-177316e4ac06>
2011-08-03 21:38:00 +0000

    Project setup in AVR studio 5

---
+++ trunk/Dev2/pushpak/pushpak.c
@@ -0,0 +1,16 @@
+/*
+ * pushpak.c
+ *
+ * Created: 8/3/2011 4:50:28 PM
+ *  Author: SandeepS
+ */ 
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
